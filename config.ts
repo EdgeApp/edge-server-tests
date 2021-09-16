6a41ce7421094682f0a6f0535a293659fc317eb4
Lex Peterson <>
2021-09-16 14:23:52 -0700

    Add config file

---
+++ config.ts
@@ -0,0 +1,28 @@
+import { makeConfig } from 'cleaner-config'
+import { asArray, asBoolean, asNumber, asObject, asOptional, asString } from 'cleaners'
+
+export const asPostmanEnvironment = asObject({
+    id: asOptional(asString, 'dd58d2e1-3c1b-417d-b3d1-d5180b7eddb8'),
+    name: asOptional(asString, 'edge-server-tests'),
+    values: asOptional(
+        asArray(
+            asObject({
+                key: asString,
+                value: asString,
+                enabled: asBoolean
+            })
+        ),
+        [
+            {
+                key: 'infoUrl',
+                value: 'https://info1.edge.app/v1',
+                enabled: true
+            }
+        ]
+    ),
+    _postman_variable_scope: asOptional(asString, "environment"),
+	_postman_exported_at: asOptional(asString, "2021-09-16T20:36:52.299Z"),
+	_postman_exported_using: asOptional(asString, "Postman/8.12.1")
+})
+
+export const config = makeConfig(asPostmanEnvironment, './environments/edge-server-tests.postman_environment.json')
